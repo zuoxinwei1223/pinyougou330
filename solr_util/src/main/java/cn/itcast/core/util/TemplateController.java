8a2b7e720318eace878eeba950cde19c659b1671
root <root@163.com>
2019-01-08 15:05:22 +0800

    新增模块业务

---
+++ solr_util/src/main/java/cn/itcast/core/util/TemplateController.java
@@ -0,0 +1,12 @@
+package cn.itcast.core.util;
+
+/**
+ * Created by Administrator on 2019/1/8.
+ */
+public class TemplateController {
+    public static void main(String[] args) {
+        System.out.print("模板模块");
+        System.out.print("模板模块");
+        System.out.print("模板模块");
+    }
+}

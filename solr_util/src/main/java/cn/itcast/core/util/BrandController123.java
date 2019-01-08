5cd7d59a18467ab36f584c306555688d585a30eb
root <root@163.com>
2019-01-08 14:45:20 +0800

    修改业务了

--- solr_util/src/main/java/cn/itcast/core/util/BrandController123.java
+++ solr_util/src/main/java/cn/itcast/core/util/BrandController123.java
@@ -5,6 +5,12 @@ package cn.itcast.core.util;
  */
 public class BrandController123 {
     public static void main(String[] args) {
+
+        System.out.print("1231231");
+        System.out.print("1231231");
+        System.out.print("1231231");
+        System.out.print("1231231");
+        System.out.print("1231231");
         System.out.print("1231231");
     }
 }

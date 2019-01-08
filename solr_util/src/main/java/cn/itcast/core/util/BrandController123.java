7e8a36696390eef72e2b44e5ba5d45cf7579835a
root <root@163.com>
2019-01-08 13:59:20 +0800

    新模块

--- solr_util/src/main/java/cn/itcast/core/util/BrandController123.java
+++ solr_util/src/main/java/cn/itcast/core/util/BrandController123.java
@@ -3,7 +3,7 @@ package cn.itcast.core.util;
 /**
  * Created by Administrator on 2019/1/8.
  */
-public class SolrImport {
+public class BrandController123 {
     public static void main(String[] args) {
         System.out.print("1231231");
     }
